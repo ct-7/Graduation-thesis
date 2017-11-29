9143fcb59f414a239e7bb7397be8729113e0ba7a
ct-7 <33247262+ct-7@users.noreply.github.com>
2017-11-29 14:07:40 +0900

    Create plot.R

---
+++ plot.R
@@ -0,0 +1,60 @@
+
+########## plot function #############
+plot_Func <- function(temp.x,temp.y,sens.loc,EStim_d,ylim,xlim){
+  sens.loc = data.frame(sens.loc)
+  
+  # temp plot
+  plot(temp.x,temp.y,col="red",cex = 0.7,pch = 19,ylim = ylim,
+       xaxt = 'n',yaxt ='n',xlab = "",ylab = "")
+  par(new = T) 
+  
+  # senser plot
+  plot(sens.loc,pch = 19,cex = 0.5,xlab = "",ylab = "",ylim = ylim)
+  text(sens.loc,labels = rownames(sens.loc),pos = 3,col="black")  #図が重なる
+  par(new = T)
+  
+}
+plot_Func(temp.x,temp.y,sens.loc,dat1,ylim = c(-1.5,1.7),xlim = c(0,3))
+
+
+
+
+##################################################
+#  function plot
+x = seq(-5,5,0.1)
+sigmoid.function <- function(x){
+  1/(1+exp(-x))
+}
+sigmoid.abs.function <- function(x){
+  1/(1+exp(abs(x)))
+}
+y = sigmoid.function(x)
+y1 = sigmoid.abs.function(x)
+
+# 累積密度関数 sigmoid
+plot(y,type="l",xlab="x",main="sigmoid function",col=4)
+plot(y1,type="l",xlab="x",main="sigmoid.abs function",col=3)
+p <- ggplot(data=data.frame(x=c(-7,7)), aes(x=x))
+p <- p + stat_function(fun=sigmoid.function)
+p
+
+# 累積密度関数 sigmoid.abs
+plot(y,type="l",xlab="x",main="sigmoid.abs function",col=4)
+p <- ggplot(data=data.frame(x=c(-7,7)), aes(x=x))
+p <- p + stat_function(fun=sigmoid.abs.function)
+p
+
+
+# 確率密度関数
+p <- ggplot(data=data.frame(x=c(-9,9)), aes(x=x))
+p <- p + theme(axis.text.x = element_text(size=15),axis.text.y = element_text(size=15))
+p <- p + theme(axis.title.x = element_text(size=15),axis.title.y = element_text(size=15))
+# p <- p + labs(title="sigmoid function")
+
+p <- p + stat_function(fun=dlogis)
+p <- p + stat_function(fun = dnorm,color = "orange")
+p
+
+install.packages("animation")
+library(animation)
+
