b532197fb29684f2dc1a002c048b398bf6d38fa9
ct-7 <33247262+ct-7@users.noreply.github.com>
2017-11-29 14:06:40 +0900

    Update circle.R

--- circle.R
+++ circle.R
@@ -3,9 +3,11 @@ plot.circle <- function(x, y, r){
   points(x + r*cos(theta), y + r*sin(theta), type="l", asp=1)
 }
 plot(c(-3,3), c(-3,3), type="n", asp=1,xaxt = "n",yaxt = "n",ylab = "",xlab = "") # 作図領域を用意
-plot.circle(0, -1, 2)       # 顔？
-plot.circle(-1.55, 1, 1.2) # 左耳？
-plot.circle( 0.8, 1.6, 1.3) # 右耳？
+plot.circle(0, -1, 2)       
+plot.circle(-1.55, 1, 1.2) 
+plot.circle( 0.8, 1.6, 1.3) 
+
+# 中心のplot
 par(new=T)
 plot(0,-1,col = "red",pch = 19,ylim = c(-3,3),xlim = c(-3,3),xaxt = "n",yaxt = "n",ylab = "",xlab = "")
 par(new=T)
