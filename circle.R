7feb371d42d9e801f3ece5fa458bdc9c544a3ab9
ct-7 <33247262+ct-7@users.noreply.github.com>
2017-11-29 14:05:59 +0900

    Create circle.R

---
+++ circle.R
@@ -0,0 +1,14 @@
+plot.circle <- function(x, y, r){
+  theta <- seq(-pi, pi, length=100)
+  points(x + r*cos(theta), y + r*sin(theta), type="l", asp=1)
+}
+plot(c(-3,3), c(-3,3), type="n", asp=1,xaxt = "n",yaxt = "n",ylab = "",xlab = "") # 作図領域を用意
+plot.circle(0, -1, 2)       # 顔？
+plot.circle(-1.55, 1, 1.2) # 左耳？
+plot.circle( 0.8, 1.6, 1.3) # 右耳？
+par(new=T)
+plot(0,-1,col = "red",pch = 19,ylim = c(-3,3),xlim = c(-3,3),xaxt = "n",yaxt = "n",ylab = "",xlab = "")
+par(new=T)
+plot(-1.1,0.95,col = "red",pch = 19,ylim = c(-3,3),xlim = c(-3,3),xaxt = "n",yaxt = "n",ylab = "",xlab = "")
+par(new=T)
+plot(0.58,1.55,col = "red",pch = 19,ylim = c(-3,3),xlim = c(-3,3),xaxt = "n",yaxt = "n",ylab = "",xlab = "")
